10b222fbec78b9f1dd5440be81e73134e9f40016
Tzuhsuan_Chen <Tzuhsuan_Chen@asus.com>
2019-10-28 18:35:40 +0800

    ft5406: Export the start polling function.
    1.Because of the timing issue, export the start polling function
      and the display driver can call it when display is ready.
    2.Add the retry mechanism.
    
    Change-Id: Ifec59e2e4600d3c4f86dd97d013a1c7fb075dfa5

--- drivers/miniarm/dsi/panel-toshiba-tc358762.c
+++ drivers/miniarm/dsi/panel-toshiba-tc358762.c
@@ -295,6 +295,7 @@ static int tc358762_prepare(struct drm_panel *panel)
 }
 
 extern void tinker_mcu_screen_power_up(void);
+extern void tinker_ft5406_start_polling(void);
 static int tc358762_enable(struct drm_panel *panel)
 {
 	struct tc358762 *p = to_tc358762(panel);
@@ -306,6 +307,8 @@ static int tc358762_enable(struct drm_panel *panel)
 
 	pr_info("tinker_mcu_screen_power_up");
 	tinker_mcu_screen_power_up();
+	msleep(100);
+	tinker_ft5406_start_polling();
 
 	tc358762_dsi_init(p);
 
